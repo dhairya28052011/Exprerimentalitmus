17731b30334a5265f6f8378b679fe2ec3ce5413f
dhairya28052011 <dhairya28052011@gmail.com>
2026-08-22 19:48:59 +0530

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,10 @@
+import ExperimentLitmus from './ExperimentLitmus';
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px', fontFamily: 'sans-serif' }}>
+      <h1>Virtual Chemistry Lab</h1>
+      <ExperimentLitmus />
+    </div>
+  );
+}
